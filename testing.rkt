b14f68cbddcfca08b09d867f5b86bbe8f7623b8f
FanC-06 <qianfan_chen@outlook.com>
2021-04-16 13:11:46 -0400

    trace

--- testing.rkt
+++ testing.rkt
@@ -288,20 +288,21 @@ pred traces{
 	init
 	/* always (addToBallpark or ballToWaiting or waitingToVac or vacToObs or obsToExit or (doNothing and not ballToWaitingGuard and not waitingToVacGuard and not vacToObsGuard and not obsToExitGuard and not makeVacGuard)) */
 
-	// // always (addToBallpark or ballToWaiting or waitingToVac or vacToObs or obsToExit or doNothing)
-	// ballToWaiting
-	// after waitingToVac
-	// after after doNothing
-	// after after after vacToObs
-	// after after after after doNothing
-	// after after after after after doNothing
-	// after after after after after after doNothing
-	// after after after after after after after doNothing
-	// after after after after after after after after obsToExit
-	// after after after after after after after after after always (doAbosolutelyNothing and no people and no NextPersonTracker.nextPerson)
+	// always (addToBallpark or ballToWaiting or waitingToVac or vacToObs or obsToExit or doNothing)
+	
+	ballToWaiting
+	after waitingToVac
+	after after doNothing
+	after after after vacToObs
+	after after after after doNothing
+	after after after after after doNothing
+	after after after after after after doNothing
+	after after after after after after after doNothing
+	after after after after after after after after obsToExit
+	after after after after after after after after after always (doAbosolutelyNothing and no people and no NextPersonTracker.nextPerson)
 
 
-	always (ballToWaiting or waitingToVac or vacToObs or obsToExit or doNothing or (doAbosolutelyNothing and no people and no NextPersonTracker.nextPerson))
+	// always (ballToWaiting or waitingToVac or vacToObs or obsToExit or doNothing or (doAbosolutelyNothing and no people and no NextPersonTracker.nextPerson))
 }
 
 run {traces} for exactly 1 Person, 5 Int
